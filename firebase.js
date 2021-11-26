657f3430979330239a0641ae223f9f5cd9d2f1f9
Axel-Makes-it <95051495+Axel-Makes-it@users.noreply.github.com>
2021-11-25 22:07:23 -0500

    netlix first commit

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC3FQTYGqJ9Ys5wwxXQr6scZKuvzvuY_6M",
+    authDomain: "netflix-clone-fb822.firebaseapp.com",
+    projectId: "netflix-clone-fb822",
+    storageBucket: "netflix-clone-fb822.appspot.com",
+    messagingSenderId: "934824231701",
+    appId: "1:934824231701:web:82517aeaba6e1a15559f1c",
+    measurementId: "G-J4GH6SMVR1"
+  };
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import HomeScreen from "./HomeScreen";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+function App() {
+  return (
+    <div className="app">
+      
+            <HomeScreen />
+           
+    </div>
+  );
+}
+
+export default App;
